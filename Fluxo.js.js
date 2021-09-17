e1195b352fb9cd6d0dd085b9523e46a8a9e120f9
wilton0321 <90014955+wilton0321@users.noreply.github.com>
2021-09-16 22:22:29 -0300

    Add files via upload

---
+++ Fluxo.js.js
@@ -0,0 +1,42 @@
+var idade = 18;
+if (idade >= 18) {
+    console.log("Informe a data do evento")
+
+}else {
+    console.log("Sem permissão")
+
+}
+var eventoData = 2022
+var DataAtual = 2021
+if (eventoData >= DataAtual) {
+console.log("Sem permissão") 
+
+}else {
+console.log("Com permissão)
+
+}
+var participantes = 100;
+if (participantes <= 100) {
+    console.log("Fim")
+
+}else {
+    console.log("Coloque o nome dos participantes")
+
+}
+var palestrantes = 8;
+if (palestrantes <= 8) {
+    console.log("Fim")
+
+}else {
+    console.log("Coloque o nome dos palestrantes")
+
+}
+var participantes = 100
+var palestrantes = 8
+if (participantes >= 100) {
+    console.log("Não permitir cadastro")
+
+}else {
+    console.log("Permitir cadastro")
+
+}
